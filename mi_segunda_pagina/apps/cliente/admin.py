0ee9d6f0d5c25dc1a006022cd63d4fab7ab1c330
santioromendia <santioromendia@gmail.com>
2024-02-07 13:56:40 -0300

    mejoras definitivas

--- mi_segunda_pagina/apps/cliente/admin.py
+++ mi_segunda_pagina/apps/cliente/admin.py
@@ -4,3 +4,4 @@ from . import models
 # Register your models here.
 admin.site.register(models.pais)
 admin.site.register(models.cliente)
+admin.site.register(models.sede)
--- mi_segunda_pagina/apps/cliente/models.py
+++ mi_segunda_pagina/apps/cliente/models.py
@@ -8,13 +8,25 @@ class pais (models.Model):
     def __str__(self):
         return self.nombre
     
+class sede (models.Model):
+    sede= models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.sede
+    
 class cliente(models.Model):
     nombre= models.CharField(max_length=100)
     apellido= models.CharField(max_length=100)
     nacimiento= models.DateField(null=True, blank=True)
-    pais_origen_id=models.ForeignKey(pais, null=True, blank=True, on_delete=models.SET_NULL)
+    pais_origen=models.ForeignKey(pais, null=True, blank=True, on_delete=models.SET_NULL)
+    ciudad=models.CharField(max_length=100, null=True, blank=True)
+    peso=models.FloatField(null=True, blank=True)
+    altura=models.FloatField(null=True, blank=True)
+    telefono=models.FloatField(null=True, blank=True)
+    mail=models.EmailField(max_length=254, null=True, blank=True)
+    sede=models.ForeignKey(sede, null=True, blank=True, on_delete=models.SET_NULL)
     
-    
+   
     def __str__(self):
         return f"{self.apellido}, {self.nombre}"
     
